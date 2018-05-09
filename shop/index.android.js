ce6e8de0a186860bd6ceb15d8347a1911238b3e7
unknown <thachcs1995@gmail.com>
2018-05-09 13:31:09 +0700

    init

---
+++ shop/index.android.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+import {AppRegistry} from 'react-native';
+
+import ShopSide from './src/index';
+
+export default class ShopSideAndroid extends Component{
+    render(){
+        return <ShopSide />
+    }
+}
